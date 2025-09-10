99d936d09b141592ceccd1b3e400778b2d92b598
yadai <yadai@amd.com>
2025-09-10 16:53:43 +0800

    update

--- fbgemm_gpu/include/fbgemm_gpu/utils/kernel_launcher.cuh
+++ fbgemm_gpu/include/fbgemm_gpu/utils/kernel_launcher.cuh
@@ -280,7 +280,7 @@ struct KernelLauncher {
         // c10::cuda::get_cuda_error_help(cuda_error),
         c10::cuda::get_cuda_check_suffix(),
 #endif
-        "\n",
+        "\n"
         // c10::cuda::c10_retrieve_device_side_assertion_info());
         );
   }
