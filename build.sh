900f8c01f50b973b225eaf7fe574a03ab898a8ef
yadai <yadai@amd.com>
2025-09-15 04:10:16 +0000

    add build

---
+++ build.sh
@@ -0,0 +1,11 @@
+o	git submodule update
+	git submodule sync --recursive
+	git submodule update --init --recursive
+	 
+	pip3 install -r requirements.txt
+	Pip3 install setuptools==75.1.0
+	 
+	cd ./fbgemm_gpu
+	 
+	export BUILD_ROCM_VERSION=7.0 && export MAX_JOBS='nproc' && gpu_arch="$(/opt/rocm/bin/rocminfo | grep -o -m 1 'gfx.*')" && export PYTORCH_ROCM_ARCH=$gpu_arch && python setup.py --build-variant rocm -DHIP_ROOT_DIR=/opt/rocm -DCMAKE_C_FLAGS="-DTORCH_USE_HIP_DSA" -DCMAKE_CXX_FLAGS="-DTORCH_USE_HIP_DSA" build develop 2>&1 | tee build.log
+ 
