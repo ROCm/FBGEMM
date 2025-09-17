a5719ed9d74f8539d4726f5969109457b9c98bee
yadai <yadai@amd.com>
2025-09-17 09:42:56 +0000

    update

---
+++ fbgemm_gpu/rebuild.sh
@@ -0,0 +1,2 @@
+export BUILD_ROCM_VERSION=7.0 && export MAX_JOBS='nproc' && gpu_arch="$(/opt/rocm/bin/rocminfo | grep -o -m 1 'gfx.*')" && export PYTORCH_ROCM_ARCH=$gpu_arch && python setup.py --build-variant rocm -DHIP_ROOT_DIR=/opt/rocm -DCMAKE_C_FLAGS="-DTORCH_USE_HIP_DSA" -DCMAKE_CXX_FLAGS="-DTORCH_USE_HIP_DSA" build develop 2>&1 | tee build.log
+ 
